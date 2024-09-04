b187f1c3e5989d4ea820f1d94ba0f2dc51699035
developkosarev <develop.kosarev@gmail.com>
2024-09-04 21:35:50 +0200

    Add tests

--- api/src/Messenger/ExternalJsonMessengerSerializer.php
+++ api/src/Messenger/ExternalJsonMessengerSerializer.php
@@ -4,12 +4,21 @@ namespace App\Messenger;
 
 use App\Message\ExternalEmail;
 use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\Exception\MessageDecodingFailedException;
 use Symfony\Component\Messenger\Transport\Serialization\SerializerInterface;
 
 class ExternalJsonMessengerSerializer implements SerializerInterface
 {
     public function decode(array $encodedEnvelope): Envelope
     {
+        //$body = $encodedEnvelope['body'];
+        //$headers = $encodedEnvelope['headers'];
+        //
+        //$data = json_decode($body, true);
+        //die(var_dump($data));
+        //$message = new ExternalEmail($data['properties'], $data['body']);
+        //return new Envelope($message);
+
         $properties = $encodedEnvelope['properties'];
         $body = $encodedEnvelope['body'];
         $data = json_decode($body, true);
@@ -20,6 +29,25 @@ class ExternalJsonMessengerSerializer implements SerializerInterface
 
     public function encode(Envelope $envelope): array
     {
-        throw new \Exception('Transport & serializer not meant for sending messages');
+        $message = $envelope->getMessage();
+        //$stamps = $envelope->all();
+
+        if (!($message instanceof ExternalEmail)) {
+            throw new \Exception(sprintf('Serializer does not support message of type %s.', $message::class));
+        }
+
+        $data = [
+            'properties' => json_encode($message->getProperties()),
+            'body' => json_encode($message->getBody())
+        ];
+
+        return [
+            'body' => json_encode($data),
+            //'headers' => [
+            //    'stamps' => serialize($stamps)
+            //]
+        ];
+
+        //throw new \Exception('Transport & serializer not meant for sending messages');
     }
 }
---
+++ api/tests/Messenger/ExternalJsonMessengerSerializerTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Tests\Messenger;
+
+use App\Email\Newsletter\SubscribeConfirm;
+use App\Message\ExternalEmail;
+use App\Messenger\ExternalJsonMessengerSerializer;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Messenger\Envelope;
+
+class ExternalJsonMessengerSerializerTest extends KernelTestCase
+{
+    private const EMAIL = 'test@test.test';
+
+    private static ExternalJsonMessengerSerializer $serializer;
+
+    public static function setUpBeforeClass(): void
+    {
+        self::bootKernel();
+        $container = static::getContainer();
+        self::$serializer = $container->get(ExternalJsonMessengerSerializer::class);
+    }
+
+    public function testDecode()
+    {
+        //‘“{\“properties\“:{\“header\“:\“email\“,\“type\“:\“NEWSLETTER_SUBSCRIBE_CONFIRM\“},\“body\“:{\“email\“:\“xx.yy@gmail.com\“,\“website_id\“:1,\“store_id\“:1,\“confirm_code\“:\“xxx\“,\“base_url\“:\“https:\\\/\\\/dev9.test.test\\\/\“,\“store_name\“:\“German\“,\“customer_name\“:\“\”}}“’
+
+        $encodedEnvelope = [
+            'properties' => [],
+            'body' => '{"properties":
+                        {"header":"email","type":"NEWSLETTER_SUBSCRIBE_CONFIRM"},
+                        "body":
+                        {"email":"xx.yy@gmail.com","website_id":1,"store_id":1,"confirm_code":"xxx","base_url":"URL","store_name":"German","customer_name":"name"}}'
+        ];
+
+        $result = self::$serializer->decode($encodedEnvelope);
+
+        $this->assertInstanceOf(Envelope::class, $result);
+    }
+
+    public function testEncode()
+    {
+        $properties = ['header' => 'email', 'type' => 'NEWSLETTER_SUBSCRIBE_CONFIRM'];
+        $data = ['email' => 'xx.yy@gmail.com'];
+
+        $message = new ExternalEmail($properties, $data);
+
+        $result = self::$serializer->encode(new Envelope($message));
+        $this->assertIsArray($result);
+    }
+}
