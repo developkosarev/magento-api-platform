f4af937c1d533744dc744b12c252e036140dcc79
developkosarev <develop.kosarev@gmail.com>
2024-09-29 12:44:05 +0200

    Add newsletter

---
+++ api/src/Entity/NewsletterSubscriber.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NewsletterSubscriberRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: NewsletterSubscriberRepository::class)]
+#[ORM\Table(name: 'sunday_newsletter_subscriber')]
+class NewsletterSubscriber
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(name: 'entity_id', type: 'integer')]
+    private ?int $id = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
---
+++ api/src/Repository/NewsletterSubsriberRepository.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\NewsletterSubscriber;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<NewsletterSubscriber>
+ *
+ * @method NewsletterSubscriber|null find($id, $lockMode = null, $lockVersion = null)
+ * @method NewsletterSubscriber|null findOneBy(array $criteria, array $orderBy = null)
+ * @method NewsletterSubscriber[]    findAll()
+ * @method NewsletterSubscriber[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class NewsletterSubsriberRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, NewsletterSubscriber::class);
+    }
+
+//    /**
+//     * @return Orders[] Returns an array of Orders objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('o')
+//            ->andWhere('o.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('o.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Orders
+//    {
+//        return $this->createQueryBuilder('o')
+//            ->andWhere('o.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
