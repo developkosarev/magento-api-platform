474337a9ec3f8f5f8c7854bd12aff02cfc7591fa
developkosarev <develop.kosarev@gmail.com>
2024-10-05 21:04:23 +0200

    Add email

--- api/src/Entity/Main/EmailLog.php
+++ api/src/Entity/Main/EmailLog.php
@@ -2,9 +2,11 @@
 
 namespace App\Entity\Main;
 
+use App\Repository\Main\EmailLogRepository;
 use Doctrine\ORM\Mapping as ORM;
+use DateTime;
 
-#[ORM\Entity]
+#[ORM\Entity(repositoryClass: EmailLogRepository::class)]
 #[ORM\Table(name: 'email_log')]
 class EmailLog
 {
@@ -13,8 +15,44 @@ class EmailLog
     #[ORM\Column(name: 'id', type: 'integer')]
     private ?int $id = null;
 
+    #[ORM\Column(name: 'created_at', type: 'datetime', nullable: true)]
+    protected ?DateTime $createdAt;
+
+    #[ORM\Column(name: 'email_type', type: 'string', length: 100)]
+    private ?string $emailType;
+
+    #[ORM\Column(name: 'quantity', type: 'integer')]
+    private int $quantity;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime();
+        $this->quantity = 1;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getCreatedAt(): ?DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function getEmailType(): ?string
+    {
+        return $this->emailType;
+    }
+
+    public function setEmailType(string $emailType): self
+    {
+        $this->emailType = $emailType;
+        return $this;
+    }
+
+    public function getQuantity(): int
+    {
+        return $this->quantity;
     }
 }
---
+++ api/src/Repository/Main/EmailLogRepository.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Repository\Main;
+
+use App\Email\EmailInterface;
+use App\Entity\Main\EmailLog;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<EmailLog>
+ *
+ * @method EmailLog|null find($id, $lockMode = null, $lockVersion = null)
+ * @method EmailLog|null findOneBy(array $criteria, array $orderBy = null)
+ * @method EmailLog[]    findAll()
+ * @method EmailLog[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class EmailLogRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, EmailLog::class);
+    }
+
+    public function add(EmailInterface $email): EmailLog
+    {
+        $emailLog = new EmailLog();
+        $emailLog->setEmailType($email->getEmailType());
+
+        $this->_em->persist($emailLog);
+
+        return $emailLog;
+    }
+
+    public function save(): void
+    {
+        $this->_em->flush();
+    }
+
+//    /**
+//     * @return Orders[] Returns an array of Orders objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('o')
+//            ->andWhere('o.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('o.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Orders
+//    {
+//        return $this->createQueryBuilder('o')
+//            ->andWhere('o.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
