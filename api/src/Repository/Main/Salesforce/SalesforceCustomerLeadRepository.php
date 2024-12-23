51ffafd838f7431369bdb3dc6ed8979018fe609b
developkosarev <develop.kosarev@gmail.com>
2024-12-23 20:19:21 +0100

    Remove comments

--- api/src/Repository/Main/Salesforce/SalesforceCustomerLeadRepository.php
+++ api/src/Repository/Main/Salesforce/SalesforceCustomerLeadRepository.php
@@ -39,29 +39,4 @@ class SalesforceCustomerLeadRepository extends ServiceEntityRepository
             ->getQuery()
             ->getResult();
     }
-
-//    /**
-//     * @return Orders[] Returns an array of Orders objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('o')
-//            ->andWhere('o.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('o.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?Orders
-//    {
-//        return $this->createQueryBuilder('o')
-//            ->andWhere('o.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
