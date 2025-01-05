76c21f4aa30991271eec3af2cb8483fadef3af7e
Dmitrii Kosarev <develop.kosarev@gmail.com>
2025-01-05 16:41:23 +0100

    Add salesforce v13 (#28)
    * Add prefix
    
    * Add bitbucket
    
    * Add bitbucket
    
    * Add bitbucket

--- api/src/Service/Salesforce/Common/Config.php
+++ api/src/Service/Salesforce/Common/Config.php
@@ -4,32 +4,8 @@ namespace App\Service\Salesforce\Common;
 
 class Config
 {
-    public function getApiOauth2Url(): string
+    public function getS3Prefix(): string
     {
-        return $_ENV["SALESFORCE_API_OAUTH2_URL"];
-    }
-
-    public function getApiWebServiceUrl(): string
-    {
-        return $_ENV["SALESFORCE_API_WEB_SERVICE_URL"];
-    }
-
-    public function getApiClientId(): string
-    {
-        return $_ENV["SALESFORCE_API_CLIENT_ID"];
-    }
-    public function getApiClientSecret(): string
-    {
-        return $_ENV["SALESFORCE_API_CLIENT_SECRET"];
-    }
-
-    public function getApiPassword(): string
-    {
-        return $_ENV["SALESFORCE_API_PASSWORD"];
-    }
-
-    public function getPrefix(): string
-    {
-        return $_ENV["SALESFORCE_API_PREFIX"];
+        return $_ENV["SALESFORCE_S3_PREFIX"];
     }
 }
