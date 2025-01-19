8caf47565a82c5e7bdea1caf0087ea0986b86300
Dmitrii Kosarev <develop.kosarev@gmail.com>
2025-01-19 17:37:38 +0100

    Add traking (#31)

---
+++ api/src/Repository/Main/Order/TrackingRepository.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Repository\Main\Order;
+
+
+use App\Entity\Main\Order\Tracking;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Tracking>
+ *
+ * @method Tracking|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Tracking|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Tracking[]    findAll()
+ * @method Tracking[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class TrackingRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Tracking::class);
+    }
+
+//    /**
+//     * @return Orders[] Returns an array of Orders objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('o')
+//            ->andWhere('o.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('o.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Orders
+//    {
+//        return $this->createQueryBuilder('o')
+//            ->andWhere('o.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
