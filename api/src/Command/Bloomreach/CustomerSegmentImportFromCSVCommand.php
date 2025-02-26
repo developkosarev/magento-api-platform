19c141cb704b0cc8c6546b4f9e1fae0ddc8dc709
developkosarev <develop.kosarev@gmail.com>
2025-02-26 11:08:34 +0100

    Change command

--- api/src/Command/Bloomreach/CustomerSegmentImportFromCSVCommand.php
+++ api/src/Command/Bloomreach/CustomerSegmentImportFromCSVCommand.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace App\Command\Bloomreach;
 
+use App\Service\Bloomreach\Customer\CustomerSegmentFileImport;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
@@ -18,10 +19,13 @@ use Symfony\Component\Console\Output\OutputInterface;
 )]
 class CustomerSegmentImportFromCSVCommand extends Command
 {
+    private const SEGMENT_ID_ARGUMENT_NAME = 'segment_id';
+    private const WEBSITE_ID_ARGUMENT_NAME = 'website_id';
     private const OPTION_FORCE = 'force';
     private bool $force;
 
     public function __construct(
+        private readonly CustomerSegmentFileImport $fileImport,
         private readonly LoggerInterface $logger,
         string $name = null
     ) {
@@ -36,6 +40,16 @@ class CustomerSegmentImportFromCSVCommand extends Command
     protected function configure(): void
     {
         $this
+            ->addArgument(
+                self::SEGMENT_ID_ARGUMENT_NAME,
+                InputArgument::REQUIRED,
+                'Segment ID for which customers should be imported'
+            )
+            ->addArgument(
+                self::WEBSITE_ID_ARGUMENT_NAME,
+                InputArgument::REQUIRED,
+                'Website ID for which customers should be imported'
+            )
             ->addOption(
                 self::OPTION_FORCE,
                 null,
@@ -46,18 +60,36 @@ class CustomerSegmentImportFromCSVCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $msg = sprintf('Start execution command "%s"', $this->getName());
+        $segmentId = (int) $input->getArgument(self::SEGMENT_ID_ARGUMENT_NAME);
+        $websiteId = (int) $input->getArgument(self::WEBSITE_ID_ARGUMENT_NAME);
+        $msg = "<info>Starting import for segment #{$segmentId} and website {$websiteId}</info>";
         $this->logger->info($msg);
         $output->writeln($msg);
+
+        $start = microtime(true);
+
+        //$this->fileImport->execute();
 
         //if ($input->getOption(self::OPTION_IMPORT_ORDERS)) {
         //    $this->importOrdersFromCSV($output);
         //}
 
-        $msg = 'Execution finished';
-        $this->logger->info($msg);
+
+        $end = microtime(true);
+        $msg = "<info>Import for segment #{$segmentId} and website {$websiteId} completed</info>";
         $output->writeln($msg);
+        $this->logger->info($msg);
+
+        $executionTime = round($end - $start, 2);
+        $output->writeln("<info>Memory: {$this->getMemoryUsage()}MB</info>", OutputInterface::VERBOSITY_DEBUG);
+        $output->writeln("<info>Time: {$executionTime}s</info>", OutputInterface::VERBOSITY_DEBUG);
 
         return Command::SUCCESS;
+    }
+
+    private function getMemoryUsage(): float
+    {
+        $memUsage = memory_get_usage(true);
+        return round($memUsage / 1048576, 2);
     }
 }
