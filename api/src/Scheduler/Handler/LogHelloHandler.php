c5a513b180a91b31a2792888239463169e535b54
Dmitrii Kosarev <develop.kosarev@gmail.com>
2025-03-08 16:55:18 +0100

    Add logs (#40)
    Co-authored-by: developkosarev <developkosarev@gmail.com>

--- api/src/Scheduler/Handler/LogHelloHandler.php
+++ api/src/Scheduler/Handler/LogHelloHandler.php
@@ -3,17 +3,31 @@
 namespace App\Scheduler\Handler;
 
 use App\Scheduler\Message\LogHello;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 use Psr\Log\LoggerInterface;
 
 #[AsMessageHandler]
 final class LogHelloHandler
 {
-    public function __construct(private LoggerInterface $logger)
+    public function __construct(
+        private readonly LoggerInterface $logger,
+        private readonly EntityManagerInterface $entityManager,
+        private readonly EntityManagerInterface $magentoEntityManager,
+    )
     {
     }
     public function __invoke(LogHello $message)
     {
+        if (!$this->entityManager->isOpen()) {
+            $this->logger->warning('Entity Manager is closed');
+        }
+        if (!$this->magentoEntityManager->isOpen()) {
+            $this->logger->warning('Magento Entity Manager is closed');
+        }
+
+        $this->logger->info(str_repeat('🎸', $message->length).' '.$message->length);
+        $this->logger->notice(str_repeat('🎸', $message->length).' '.$message->length);
         $this->logger->warning(str_repeat('🎸', $message->length).' '.$message->length);
     }
 }
