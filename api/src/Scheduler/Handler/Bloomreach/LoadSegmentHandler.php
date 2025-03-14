95bc096c41321c5c6fce5c3df38adca6c078a5a2
developkosarev <developkosarev@gmail.com>
2025-03-14 14:02:05 +0100

    Add schedule

--- api/src/Scheduler/Handler/Bloomreach/LoadSegmentHandler.php
+++ api/src/Scheduler/Handler/Bloomreach/LoadSegmentHandler.php
@@ -3,6 +3,8 @@
 namespace App\Scheduler\Handler\Bloomreach;
 
 use App\Scheduler\Message\Bloomreach\LoadSegment;
+use App\Service\Bloomreach\Customer\CustomerSegmentFileImport;
+use App\Service\Bloomreach\Customer\CustomerSegmentImportInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
@@ -10,12 +12,29 @@ use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 final class LoadSegmentHandler
 {
     public function __construct(
+        private readonly CustomerSegmentFileImport $fileImport,
+        private readonly CustomerSegmentImportInterface $customerSegmentImport,
         private readonly LoggerInterface $logger
     )
     {
     }
     public function __invoke(LoadSegment $message)
     {
-        $this->logger->warning(str_repeat('🤎', 1) . ' Load segments');
+        $segmentId = 2;
+        $websiteId = 1;
+
+        $msg = " Load segments #{$segmentId} website #{$websiteId}";
+        $this->logger->warning(str_repeat('🤎', 1) . $msg);
+
+        if ($this->fileImport->uploadFile($segmentId)) {
+            $fileNameLocal = $this->fileImport->getFileNameLocal($segmentId);
+
+            $this->customerSegmentImport->setForce(false);
+            $this->customerSegmentImport->execute($segmentId, $websiteId, $fileNameLocal);
+
+            if (file_exists($fileNameLocal)) {
+                unlink($fileNameLocal);
+            }
+        }
     }
 }
