e7e94c43c2302cdaeb8f605886ded975ae355648
developkosarev <developkosarev@gmail.com>
2025-04-08 11:55:56 +0200

    Add csheduler

--- api/src/Scheduler/Config.php
+++ api/src/Scheduler/Config.php
@@ -4,6 +4,16 @@ namespace App\Scheduler;
 
 class Config
 {
+    public function getSchedulerHello(): string
+    {
+        return $_ENV["SCHEDULER_HELLO"];
+    }
+
+    public function getSchedulerLeadCustomer(): string
+    {
+        return $_ENV["SCHEDULER_LEAD_CUSTOMER"];
+    }
+
     public function getSchedulerLoadSegment(): string
     {
         return $_ENV["SCHEDULER_LOAD_SEGMENT"];
--- api/src/Scheduler/MainSchedule.php
+++ api/src/Scheduler/MainSchedule.php
@@ -21,8 +21,8 @@ class MainSchedule implements ScheduleProviderInterface
     public function getSchedule(): Schedule
     {
         return (new Schedule())->add(
-            RecurringMessage::every('60 seconds', new LogHello(4)),
-            RecurringMessage::every('3600 seconds', new LeadCustomer()), //1 hours
+            RecurringMessage::every($this->config->getSchedulerHello(), new LogHello(4)),
+            RecurringMessage::every($this->config->getSchedulerLeadCustomer(), new LeadCustomer()), //1 hours
 
             RecurringMessage::every($this->config->getSchedulerLoadSegment(), new LoadSegment()) //82800 seconds=23 hours
             //RecurringMessage::cron('0 10 * * *', new LoadSegment()) //10:00 //composer require dragonmantank/cron-expression
